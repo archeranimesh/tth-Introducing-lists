8af65402c6d2414900879d5391d89adc2a12d8da
Animesh Bhadra <animeshb@archerimagine.com>
2020-11-11 23:37:05 +0530

    📦 NEW: Base Version of the repo.

---
+++ SRC/Chapter_01-Meet-Lists/01_list_creation.py
@@ -0,0 +1,17 @@
+a = []
+print(type(a))
+
+b = ["Python", "JavaScript", "Java"]
+print(b)
+
+languages = ["C#", "Ruby", "Swift"]
+print(languages)
+print(len(languages))
+
+
+# There should be one and only one way to do it.
+# Boolean Values
+print(bool(languages))
+print(bool([]))  # Empty list is false.
+
+# List are mutable unlike string.
